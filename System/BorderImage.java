eb9e6850cc1cc1322de5c43e8fa9845e612ca9c8
Nguyễn Vũ Mạnh Hà <20130243@st.hcmuaf.edu.vn>
2022-12-09 09:26:26 +0700

    Update BorderImage.java

--- System/BorderImage.java
+++ System/BorderImage.java
@@ -31,7 +31,7 @@ public class BorderImage {
 
 		Mat cannyOutput = new Mat();
 		Imgproc.Canny(srcGray, cannyOutput, threshold, threshold * 2); // tìm các cạnh
-		Mat drawing = Mat.zeros(cannyOutput.size(), CvType.CV_8UC3); // tạo 1 mat là khung 
+		Mat drawing = Mat.zeros(cannyOutput.size(), CvType.CV_8UC3); // tạo 1 mat là khung
 		// chuỗi các contour
 		List<MatOfPoint> contours = new ArrayList<>();
 		Mat hierarchy = new Mat();
@@ -52,10 +52,10 @@ public class BorderImage {
 				}
 			}
 		});
- 
+
 		// list hình đa giác 4 cạnh
 		List<MatOfPoint2f> screenCnt = new ArrayList<MatOfPoint2f>();
- 
+
 		for (int i = 0; i < 10; i++) {
 			// Chu vi
 			double peri = Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), false);
@@ -65,20 +65,30 @@ public class BorderImage {
 			Rect boundingRect = Imgproc.boundingRect(approx);
 			double ratio = boundingRect.width / boundingRect.height;
 			if (approx.size().height == 4) {
- 
-				screenCnt.add(approx); 
+
+				screenCnt.add(approx);
 				Scalar color = new Scalar(rng.nextInt(256), rng.nextInt(256), rng.nextInt(256));
 				Imgproc.drawContours(drawing, contours, i, color, 2, Imgproc.LINE_8, hierarchy, 0, new Point());
 			}
-		} 
-		for (MatOfPoint2f cnt : screenCnt) { 
-			System.out.println(2);
-			  System.out.println(cnt.cols());
-			  System.out.println(cnt.rows()); 
 		}
- 
+		for (MatOfPoint2f cnt : screenCnt) {
+			System.out.println(2);
+			System.out.println(cnt.height());
+			System.out.println(cnt.size());
+
+		}
 
 		Imgcodecs.imwrite("C:\\Users\\USER\\Downloads\\drawing.jpg", drawing);
+	}
+
+	public void getCharacter(Mat drawing) {
+		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+		Mat thre_mor = null;
+		// drawing chua thay bang anh da xoay va crop
+		Imgproc.morphologyEx(drawing, thre_mor, threshold, kernel);
+		List<MatOfPoint> contours = new ArrayList<>();
+		Mat hierarchy = new Mat();
+		Imgproc.findContours(drawing, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 	}
 
 	public static void main(String[] args) {
