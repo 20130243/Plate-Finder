27b6592c42692b5c7695aa6710b4d3007da25b77
Nguyễn Vũ Mạnh Hà <20130243@st.hcmuaf.edu.vn>
2022-12-09 15:10:31 +0700

    update

--- System/BorderImage.java
+++ System/BorderImage.java
@@ -28,6 +28,9 @@ public class BorderImage {
 	private double Max_char = 0.09;
 	private Random rng = new Random(12345);
 
+	private int RESIZED_IMAGE_WIDTH = 20;
+	private int RESIZED_IMAGE_HEIGHT = 30;
+
 	public void Border(Mat ThImage) {
 
 		Mat srcGray = new Mat();
@@ -78,39 +81,38 @@ public class BorderImage {
 			}
 		}
 		System.out.println(screenCnt.size());
-		// Lấy ra 4 cái đỉnh 
+		// Lấy ra 4 cái đỉnh
 		for (MatOfPoint2f cnt : screenCnt) {
 			System.out.println(cnt.height());
-				Point[] p = cnt.toArray();
-				for (Point a : p) {
-					System.out.println(a.x + " " + a.y); 
-				}
-				System.out.println(p);
-				Point p1 = p[0];
-				Point p2 = p[1];
-				Point p3 = p[2];
-				Point p4 = p[3];
-				
-				double x1= p1.x;
-				double y1=  p1.y;
-				
-				double x2=  p2.x;
-				double y2=  p2.y;
-				
-				double x3= p3.x;
-				double y3= p3.y;
-				
-				double x4= p4.x;
-				double y4= p4.y;
-				
-				ArrayList<Point> points = new ArrayList<Point>();
-				
-				
-				System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + x3 + " " + y3 + " " + x4 + " " + y4);
+
+			Point[] p = cnt.toArray();
+			for (Point a : p) {
+				System.out.println(a.x + " " + a.y);
+			}
+			System.out.println(p);
+			Point p1 = p[0];
+			Point p2 = p[1];
+			Point p3 = p[2];
+			Point p4 = p[3];
+
+			double x1 = p1.x;
+			double y1 = p1.y;
+
+			double x2 = p2.x;
+			double y2 = p2.y;
+
+			double x3 = p3.x;
+			double y3 = p3.y;
+
+			double x4 = p4.x;
+			double y4 = p4.y;
+
+			ArrayList<Point> points = new ArrayList<Point>();
+
+			System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + x3 + " " + y3 + " " + x4 + " " + y4);
+
 		}
-	
-		
-		
+
 //		getCharacter(drawing);
 
 		Imgcodecs.imwrite("D:\\ki5\\AI\\AI\\Giuaki\\Image\\test123.jpg", drawing);
@@ -127,7 +129,9 @@ public class BorderImage {
 		int roiarea = roi.width() * roi.height();
 		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
 		Mat thre_mor = new Mat();
+
 		// drawing chua thay bang anh da xoay va crop
+		// tim contour cua cac ki tu
 		Imgproc.morphologyEx(drawing, thre_mor, Imgproc.MORPH_DILATE, kernel);
 		List<MatOfPoint> contours = new ArrayList<>();
 		Mat hierarchy = new Mat();
@@ -137,16 +141,20 @@ public class BorderImage {
 		Map<Integer, Integer> char_x_ind = new HashMap<Integer, Integer>();
 		for (int i = 0; i < contours.size(); i++) {
 
+			// ve contour cho tung ki tu
 			Scalar color = new Scalar(rng.nextInt(256), rng.nextInt(256), rng.nextInt(256));
 			Imgproc.drawContours(drawing, contours, i, color, 2, Imgproc.LINE_8, hierarchy, 0, new Point());
 
 			Rect rectangle = Imgproc.boundingRect(contours.get(i));
 			int x = rectangle.x;
 			int y = rectangle.y;
+
 			int w = rectangle.width;
 			int h = rectangle.height;
+
 			double ratiochar = w / h;
 			double char_area = w * h;
+
 			if (Min_char * roiarea < char_area && char_area < Max_char * roiarea && 0.25 < ratiochar
 					&& ratiochar < 0.7) {
 				if (char_x.contains(x)) {
@@ -156,6 +164,8 @@ public class BorderImage {
 				char_x_ind.put(x, i);
 
 			}
+
+			// nhan dang ki tu
 			Collections.sort(char_x, new Comparator<Integer>() {
 				@Override
 				public int compare(Integer o1, Integer o2) {
@@ -170,6 +180,7 @@ public class BorderImage {
 					}
 				}
 			});
+
 			String strFinalString = "";
 			String first_line = "";
 			String second_line = "";
@@ -179,6 +190,14 @@ public class BorderImage {
 				y = rectangle.y;
 				w = rectangle.width;
 				h = rectangle.height;
+
+				Imgproc.rectangle(roi, new Point(x, y), new Point(x + w, y + h), color, j);
+				Mat imgROI = thre_mor.submat(y, y + w, x, x + h);
+
+				Mat imgROIResize = new Mat();
+				Imgproc.resize(imgROI, imgROIResize, new Size(RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT));
+				imgROIResize.reshape(1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT);
+
 			}
 
 		}
